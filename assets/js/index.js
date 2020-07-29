7e1d28b721298c53f6d44d5c0b77b7691cc1e249
sun0123456789 <1771139699@qq.com>
2020-07-29 17:10:10 +0800

    完善首页功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,37 @@
 $(function () {
   $(".logout").on("click", function () {
-    if (confirm("确定要退出吗?")) {
-      location.href = "login.html";
-      localStorage.removeItem("token");
-    }
+    layui.layer.confirm(
+      "确定要退出登录吗?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+
+        layui.layer.close(index);
+      }
+    );
   });
+  $.ajax({
+    url: "/my/userinfo",
+    type: "get",
+    success: function (res) {
+      // console.log(res);
+      if (res.status !== 0) {
+        return layui.layer.msg("获取信息失败");
+      }
+      load(res);
+    },
+  });
+  function load(res) {
+    var name = res.data.nickname || res.data.username;
+    $(".myname").html("欢迎 " + res.data.nickname);
+    if (res.data.user_pic !== "") {
+      $(".avatar").hide();
+      $(".layui-nav-img").attr("src", res.data.user_pic).show();
+    } else {
+      $(".avatar").html(name[0].toUpperCase()).show();
+      $(".layui-nav-img").hide();
+    }
+  }
 });
